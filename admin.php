92bd3f0dc6b60d394f83d52a4b64e7e506f53e9f
Tristan Jugapuu <52077671+tristan860@users.noreply.github.com>
2025-06-11 12:52:18 +0300

    dsaasdgfrgrefg

--- admin.php
+++ admin.php
@@ -8,8 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   </head>
   <body>
+    <div class="container">
     <form action="logout.php" method="post">
 	<input type="submit" value="Logi välja" name="logout">
+</div>
 </form>
     <?php
       session_start();
--- login.php
+++ login.php
@@ -4,50 +4,46 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>document</title>
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   </head>
   <body>
+
   <?php
   	session_start();
 	include("config.php");?>
-	<?php
-	if (isset($_SESSION['tuvastamine'])) {
-	  header('Location: admin.php');
-	  exit();
-	  }
-	if (!empty($_POST['login']) && !empty($_POST['pass'])) {
+	<div class="container">
+		<?php
+		if (isset($_SESSION['tuvastamine'])) {
+		header('Location: admin.php');
+		exit();
+		}
+		if (!empty($_POST['login']) && !empty($_POST['pass'])) {
 
 
-		$login = htmlspecialchars(trim($_POST['login']));
-		$pass = htmlspecialchars(trim($_POST['pass']));
+			$login = htmlspecialchars(trim($_POST['login']));
+			$pass = htmlspecialchars(trim($_POST['pass']));
 
-		$paring = "SELECT * FROM users WHERE user='$login'";
-      	$valjund = mysqli_query($yhendus, $paring);
+			$paring = "SELECT * FROM users WHERE user='$login'";
+			$valjund = mysqli_query($yhendus, $paring);
 
-		if ($valjund && mysqli_num_rows($valjund) == 1) {
-			$user = mysqli_fetch_assoc($valjund);
-			if (password_verify($pass, $user['password'])) {
-				$_SESSION['tuvastamine'] = 'misiganes';
-				header('Location: admin.php');
-				exit();
-			} else {
-				echo "kasutaja või parool on vale";
-			}
-}
-elseif (isset($_POST['remember']) && $_POST['remember'] == 'on') {
-				// Määrame küpsise, mis kehtib 30 päeva
-				setcookie("nimi", $login, time() + (86400 * 30), "/");
-			} else {
-				// Küpsist ei seata
-				setcookie("nimi", "", time() - 3600, "/");
-			}<
-}
-?>
-<h1>Login</h1>
-<form action="" method="post">
-	Login: <input type="text" name="login"><br>
-	Password: <input type="password" name="pass"><br>
-	<input type="submit" value="Logi sisse">
-	<input type="checkbox" class="mt-3" checked name="remember" > Jäta meelde
-</form>
+			if ($valjund && mysqli_num_rows($valjund) == 1) {
+				$user = mysqli_fetch_assoc($valjund);
+				if (password_verify($pass, $user['password'])) {
+					$_SESSION['tuvastamine'] = 'misiganes';
+					header('Location: admin.php');
+					exit();
+				} else {
+					echo "kasutaja või parool on vale";
+				}
+		}
+		}
+		?>
+		<h1>Login</h1>
+		<form action="" method="post">
+			Login: <input type="text" name="login"><br>
+			Password: <input type="password" name="pass"><br>
+			<input type="submit" value="Logi sisse">
+		</form>
+	</div>
 </body>
 </html>
